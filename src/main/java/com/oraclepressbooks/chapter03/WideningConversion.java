1c9704ddf08ab8d5768928458c010a2e19284e3f
sepiggy <jms1209@qq.com>
2017-01-22 15:30:49 +0800

    p48

---
+++ src/main/java/com/oraclepressbooks/chapter03/WideningConversion.java
@@ -0,0 +1,23 @@
+package com.oraclepressbooks.chapter03;
+
+/**
+ * @formatter:off
+ * WideningConversion.java
+ * 2017-01-22 14:57:57 
+ * @author JiaMingsheng
+ * p48
+ * @formatter:on
+ */
+public class WideningConversion {
+  public static void main(String[] args) {
+    byte b = 123;
+    short s = 123;
+    long l = 123;
+    char c = 123;
+
+    System.out.println("b=" + b);
+    System.out.println("s=" + s);
+    System.out.println("l=" + l);
+    System.out.println("c=" + c);
+  }
+}

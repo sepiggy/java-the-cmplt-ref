e5385765b60e0070d7b8b4085372eac7151c9bd1
sepiggy <jms1209@qq.com>
2017-01-22 14:07:55 +0800

    p49

---
+++ src/main/java/com/oraclepressbooks/chapter03/CharDemo2.java
@@ -0,0 +1,22 @@
+package com.oraclepressbooks.chapter03;
+
+/**
+ * @formatter:off
+ * CharDemo2.java
+ * 2017-01-22 08:43:44
+ * @author JiaMingsheng
+ * p40
+ * char variables behave like integers.
+ * @formatter:on
+ */
+public class CharDemo2 {
+  public static void main(String[] args) {
+    char ch1;
+
+    ch1 = 'X';
+    System.out.println("ch1 contains " + ch1);
+
+    ch1++;
+    System.out.println("ch1 is now " + ch1);
+  }
+}
